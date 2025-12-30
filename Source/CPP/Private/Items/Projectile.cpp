4ceaffa6a3d99433e1c41ef8e9edb67f7bebb1e3
torasandhi <torasandhipakey@gmail.com>
2025-12-30 21:37:48 +0800

    i hate physics

--- Source/CPP/Private/Items/Projectile.cpp
+++ Source/CPP/Private/Items/Projectile.cpp
@@ -1,79 +1,129 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Items/Projectile.h"
 #include "ObjectPoolSubsystem.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Controller.h"
+#include "Engine/World.h"
 
 AProjectile::AProjectile()
 {
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+    SphereCollider = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollider"));
+    SphereCollider->SetupAttachment(RootComponent);
+    SphereCollider->SetWorldScale3D(FVector(3.f,3.f,3.f));
 }
 
 void AProjectile::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
+    if (SphereCollider)
+    {
+       SphereCollider->OnComponentBeginOverlap.AddDynamic(this, &AProjectile::DealDamage);
+    }
 }
 
 void AProjectile::Activate(const FVector& Direction, AActor* InOwner)
 {
-	SetOwner(InOwner);                 
-	MoveDirection = Direction.GetSafeNormal();
-	AliveTime = 0.f;
+    SetOwner(InOwner);
+    MoveDirection = Direction.GetSafeNormal();
+
+    SetActorHiddenInGame(false);
+    SetActorTickEnabled(true);
 }
 
 void AProjectile::OnSpawnFromPool_Implementation()
 {
-	IPoolableInterface::OnSpawnFromPool_Implementation();
+    IPoolableInterface::OnSpawnFromPool_Implementation();
+    AliveTime = 0.f;
 }
 
 IPoolableInterface::FOnReturnedToPool& AProjectile::OnReturnedToPool()
 {
-	return ReturnToPool;
+    return ReturnToPool;
 }
 
 void AProjectile::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
-	AliveTime += DeltaTime;
-	if (AliveTime >= LifeTime)
-	{
-		if (UObjectPoolSubsystem* Pool =
-			GetWorld()->GetSubsystem<UObjectPoolSubsystem>())
-		{
-			Pool->ReturnActorToPool(this);
-		}
-		return;
-	}
+    AliveTime += DeltaTime;
+    if (AliveTime >= LifeTime)
+    {
+       ReturnActorToPool();
+       return;
+    }
 
-	const FVector Start = GetActorLocation();
-	const FVector End = Start + MoveDirection * Speed * DeltaTime;
+    const FVector Start = GetActorLocation();
+    const FVector End = Start + MoveDirection * Speed * DeltaTime;
 
-	FHitResult Hit;
-	FCollisionQueryParams Params;
-	Params.AddIgnoredActor(this);
-	Params.AddIgnoredActor(GetOwner());
+    FHitResult Hit;
+    FCollisionQueryParams Params;
+    Params.AddIgnoredActor(this);
+    
+    if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+    {
+        Params.AddIgnoredActor(PC->GetPawn());
+    }
 
-	if (GetWorld()->LineTraceSingleByChannel(
-		Hit,
-		Start,
-		End,
-		ECC_Visibility,
-		Params
-	))
-	{
-		SetActorLocation(Hit.ImpactPoint);
+    if (GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params))
+    {
+       SetActorLocation(Hit.ImpactPoint);
+       
+       AActor* HitActor = Hit.GetActor();
+       if (HitActor && !HitActor->ActorHasTag("Player"))
+       {
+           UGameplayStatics::ApplyDamage(
+               HitActor,
+               BaseDamage,
+               nullptr,
+               nullptr,
+               UDamageType::StaticClass()
+           );
+       }
 
-		if (UObjectPoolSubsystem* Pool =
-			GetWorld()->GetSubsystem<UObjectPoolSubsystem>())
-		{
-			Pool->ReturnActorToPool(this);
-		}
-		return;
-	}
+       ReturnActorToPool();
+       return;
+    }
 
-	SetActorLocation(End);
+    SetActorLocation(End);
 }
 
+void AProjectile::DealDamage(
+    UPrimitiveComponent* OverlappedComp,
+    AActor* OtherActor,
+    UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex,
+    bool bFromSweep,
+    const FHitResult& SweepResult)
+{
+    if (OtherActor == this) return;
 
-// OnOverlapBegin -> ApplyDamage -> Return To Pool
+    if (OtherActor->ActorHasTag("Player")) return;
+    
+    if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+    {
+        if (OtherActor == PC->GetPawn()) return;
+    }
+
+    UGameplayStatics::ApplyDamage(
+       OtherActor,
+       BaseDamage,
+       nullptr,
+       nullptr,
+       UDamageType::StaticClass()
+    );
+
+    ReturnActorToPool();
+}
+
+void AProjectile::ReturnActorToPool()
+{
+    SetActorTickEnabled(false);
+    SetActorHiddenInGame(true);
+
+    if (UObjectPoolSubsystem* Pool = GetWorld()->GetSubsystem<UObjectPoolSubsystem>())
+    {
+       Pool->ReturnActorToPool(this);
+    }
+}
--- Source/CPP/Public/Items/Projectile.h
+++ Source/CPP/Public/Items/Projectile.h
@@ -8,6 +8,7 @@
 #include "Projectile.generated.h"
 
 class UProjectileMovementComponent;
+class USphereComponent;
 
 UCLASS()
 class CPP_API AProjectile : public AActor, public IPoolableInterface
@@ -20,19 +21,27 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void BeginPlay() override;
 
-	void Activate(const FVector& InDirection, AActor* Owner);
+	virtual void Activate(const FVector& InDirection, AActor* Owner);
 	virtual void OnSpawnFromPool_Implementation() override;
 	virtual FOnReturnedToPool& OnReturnedToPool() override;
+	UFUNCTION()
+	virtual void DealDamage(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                        int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void ReturnActorToPool();
 
-	
+	UPROPERTY()
+	USphereComponent* SphereCollider;
+	UPROPERTY(EditDefaultsOnly, Category = "Projectile")
+	float BaseDamage = 10.f;
 	UPROPERTY(EditDefaultsOnly, Category="Projectile Settings")
 	float Speed = 6000.f;
 	UPROPERTY(EditDefaultsOnly, Category="Projectile Settings")
 	float LifeTime = 2.f;
-
 	float AliveTime = 0.f;
 
-private:
+protected:
+
 	FVector MoveDirection = FVector::ZeroVector;
 	FOnReturnedToPool ReturnToPool;
 };
